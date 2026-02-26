87ecf48c8afee31d6796154c5c57ba21f45694a2
Brian Lucas <brianlucas67@gmail.com>
2026-02-25 20:25:56 -0400

    feat: update HTML structure in app.blade.php with localized language, title, and meta tags

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="pt-BR">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>{{ config('app.name') }}</title>
+        <meta name="description" content="Simulador de elevador para teste de lógica de programação.">
+        <link rel="icon" href="{{ asset('catalise-icon.png') }}" type="image/png">
         @vite('resources/css/app.css')
     </head>
     <body>
